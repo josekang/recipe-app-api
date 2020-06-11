b0259b5398c1c339351a0fcbee026c8647c7abc6
jose <josekangethe2@gmail.com>
2020-06-11 18:48:12 +0300

    Added the user tags API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,7 +7,7 @@ from recipe import serializers
 
 # Create your views here.
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
@@ -16,3 +16,6 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
